6dfe08a6c17ea6d8027f0152eb5a3b1c89ae97ff
Manibharathi <manibharathidct@gmail.com>
2024-06-04 21:22:35 +0530

    Add Password Hash Services Test

---
+++ test/UnitTest/Services/PasswordHashServicesTest/PasswordHashServiceTest.cs
@@ -0,0 +1,65 @@
+﻿using API.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.Services.PasswordHashServicesTest
+{
+    [TestFixture]
+    internal class PasswordHashServiceTest
+    {
+        private PasswordHashService _passwordHashService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _passwordHashService = new PasswordHashService();
+
+        }
+
+        [Test]
+        public void HashPassword_WhenCalled_ReturnsHashedPassword()
+        {
+            // Arrange
+            var password = "password";
+
+            // Act
+            var result = _passwordHashService.Hash(password);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreNotEqual(password, result);
+        }
+
+        [Test]
+        public void VerifyPassword_WhenCalledWithCorrectPassword_ReturnsTrue()
+        {
+            // Arrange
+            var password = "password";
+            var hashedPassword = _passwordHashService.Hash(password);
+
+            // Act
+            var result = _passwordHashService.Verify(password, hashedPassword);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void VerifyPassword_WhenCalledWithIncorrectPassword_ReturnsFalse()
+        {
+            // Arrange
+            var password = "password";
+            var hashedPassword = _passwordHashService.Hash(password);
+
+            // Act
+            var result = _passwordHashService.Verify("wrongPassword", hashedPassword);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+    }
+}
