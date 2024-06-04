94cbec64c096ef883a33cfb5a6ccc8ea1043181f
Manibharathi <manibharathidct@gmail.com>
2024-06-04 21:05:12 +0530

    Setup the Service test

---
+++ test/UnitTest/Services/CustomerOrderServices/CustomerOrderServiceTest.cs
@@ -0,0 +1,41 @@
+﻿using API.Repositories;
+using API.Repositories.Interfaces;
+using API.Services;
+using API.Services.Interfaces;
+using AutoMapper;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.Services.CustomerOrderServices
+{
+    [TestFixture]
+    internal class CustomerOrderServiceTest : ServicesTestBase
+    {
+        private ICustomerOrderRepository _customerOrderRepository;
+        private IProductRepository _productRepository;
+        private IRestaurantRepository _restaurantRepository;
+        private IOnlinePaymentRepository _onlinePaymentsRepository;
+        private ICashPaymentRepository _cashPaymentRepository;
+        private ICustomerAddressRepository _customerAddressRepository;
+        private ICustomerOrderService _customerOrderService;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            await base.Setup();
+
+            _customerOrderRepository = new CustomerOrderRepository(_context);
+            _productRepository = new ProductRepository(_context);
+            _restaurantRepository = new RestaurantRepository(_context);
+            _onlinePaymentsRepository = new OnlinePaymentRepository(_context);
+            _cashPaymentRepository = new CashPaymentRepository(_context);
+            _customerAddressRepository = new CustomerAddressRespository(_context);
+            _customerOrderService = new CustomerOrderService(_customerOrderRepository, _productRepository, _restaurantRepository, _onlinePaymentsRepository, _cashPaymentRepository, _customerAddressRepository, _mapper);
+        }
+
+    }
+}
---
+++ test/UnitTest/Services/ServicesTestBase.cs
@@ -0,0 +1,39 @@
+﻿using API.Repositories.Interfaces;
+using API.Utility;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTest.Repositories;
+
+namespace UnitTest.Services
+{
+    internal class ServicesTestBase : RepositoryTestBase
+    {
+        
+        public IMapper _mapper;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            // Calling the base setup method
+            await base.Setup();
+            // Creating a mapper configuration
+            _mapper = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            }).CreateMapper();
+
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await base.TearDown();
+        }
+
+    }
+
+}
