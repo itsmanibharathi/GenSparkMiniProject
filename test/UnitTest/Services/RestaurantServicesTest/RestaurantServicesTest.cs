e2cd2b71617fd2a9e3e6425b9fc78739228513c0
Manibharathi <manibharathidct@gmail.com>
2024-06-04 22:24:50 +0530

    Add Restaurant Auth Service Test

---
+++ test/UnitTest/Services/RestaurantServicesTest/RestaurantServicesTest.cs
@@ -0,0 +1,161 @@
+﻿using API.Exceptions;
+using API.Models;
+using API.Models.DTOs.RestaurantDto;
+using API.Models.Enums;
+using API.Repositories.Interfaces;
+using API.Services;
+using API.Services.Interfaces;
+using AutoMapper;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.Services.RestaurantServicesTest
+{
+    [TestFixture]
+    internal class RestaurantServicesTest : ServicesTestBase
+    {
+        private RestaurantAuthService _restaurantAuthService;
+
+        [SetUp]
+        public void Setup()
+        {
+            SetupPasswordHashServices();
+            SetupRestaurantTokenService();
+            SetupRestaurantRepository();
+            _restaurantAuthService = new RestaurantAuthService(_restaurantRepository,_passwordHashService,_restaurantTokenService,_mapper );
+        }
+
+        [Test]
+        public async Task RegisterRestaurant()
+        {
+            // Arrange
+            var res = new RestaurantRegisterDto()
+            {
+                Name = "KFC",
+                Description = "KFC",
+                Email = "kfc@gmail.com",
+                Password = "password",
+                Phone = "123456789",
+                FssaiLicenseNumber = 1,
+                Address = "KFC, Bangalore",
+                Branch ="Main",
+                State = "Karnataka",
+                Zip = "",
+                City = "Bangalore",
+                AddressCode = AddressCode.a
+            };
+
+            // Act
+            var result = await _restaurantAuthService.Register(res);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.RestaurantId ==1);
+        }
+
+        [Test]
+        public async Task LoginRestaurant()
+        {
+            // Arrange
+            var res = new RestaurantRegisterDto()
+            {
+                Name = "KFC",
+                Description = "KFC",
+                Email = "kfc@gmail.com",
+                Password = "password",
+                Phone = "123456789",
+                FssaiLicenseNumber = 1,
+                Address = "KFC, Bangalore",
+                Branch = "Main",
+                State = "Karnataka",
+                Zip = "",
+                City = "Bangalore",
+                AddressCode = AddressCode.a
+            };
+            var result = await _restaurantAuthService.Register(res);
+
+
+            // Act
+            var login = new RestaurantLoginDto()
+            {
+                Email = "kfc@gmail.com",
+                Password = "password"
+            };
+            var loginResult = await _restaurantAuthService.Login(login);
+
+            // Assert
+            Assert.IsNotNull(loginResult);
+        }
+
+        [Test]
+        public async Task LoginRestauranInvalidUserExcption()
+        {
+            // Arrange
+            var res = new RestaurantRegisterDto()
+            {
+                Name = "KFC",
+                Description = "KFC",
+                Email = "kfc@gmail.com",
+                Password = "password",
+                Phone = "123456789",
+                FssaiLicenseNumber = 1,
+                Address = "KFC, Bangalore",
+                Branch = "Main",
+                State = "Karnataka",
+                Zip = "",
+                City = "Bangalore",
+                AddressCode = AddressCode.a
+            };
+            var result = await _restaurantAuthService.Register(res);
+
+
+            // Act 
+            var login = new RestaurantLoginDto()
+            {
+                Email = "kfc@kfc.in",
+                Password = "password"
+            };
+
+            // Assert
+            Assert.ThrowsAsync<InvalidUserCredentialException>(async () => await _restaurantAuthService.Login(login));
+        }
+
+
+        [Test]
+        public async Task LoginRestauranInvalidUserExcption2()
+        {
+            // Arrange
+            var res = new RestaurantRegisterDto()
+            {
+                Name = "KFC",
+                Description = "KFC",
+                Email = "kfc@gmail.com",
+                Password = "password",
+                Phone = "123456789",
+                FssaiLicenseNumber = 1,
+                Address = "KFC, Bangalore",
+                Branch = "Main",
+                State = "Karnataka",
+                Zip = "",
+                City = "Bangalore",
+                AddressCode = AddressCode.a
+            };
+            var result = await _restaurantAuthService.Register(res);
+
+
+            // Act 
+            var login = new RestaurantLoginDto()
+            {
+                Email = "kfc@gmail.com",
+                Password = "passwor"
+            };
+
+            // Assert
+            Assert.ThrowsAsync<InvalidUserCredentialException>(async () => await _restaurantAuthService.Login(login));
+        }
+    }
+}
--- test/UnitTest/Services/ServicesTestBase.cs
+++ test/UnitTest/Services/ServicesTestBase.cs
@@ -1,18 +1,28 @@
-﻿using API.Repositories.Interfaces;
+﻿using API.Models;
+using API.Repositories.Interfaces;
+using API.Services;
+using API.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
 using API.Utility;
 using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnitTest.Repositories;
+using API.Repositories;
 
 namespace UnitTest.Services
 {
     internal class ServicesTestBase : RepositoryTestBase
     {
-        
+        public IRestaurantOrderRepository _restaurantOrderRepository;
+        public IRestaurantRepository _restaurantRepository;
+        public IPasswordHashService _passwordHashService;
+        public ITokenService<Restaurant> _restaurantTokenService;
         public IMapper _mapper;
 
         [SetUp]
@@ -25,7 +35,21 @@ namespace UnitTest.Services
             {
                 cfg.AddProfile<MappingProfile>();
             }).CreateMapper();
+        }
+        public void SetupPasswordHashServices()
+        {
+            _passwordHashService = new PasswordHashService();
+        }
 
+        public void SetupRestaurantTokenService()
+        {
+            var configuration = new Mock<IConfiguration>();
+            configuration.Setup(x => x["Jwt:RestaurantSecret"]).Returns("This is the dummy key which has to be a bit long for the 512. which should be even more longer for the passing");
+            _restaurantTokenService = new RestaurantTokenService(configuration.Object);
+        }
+        public void SetupRestaurantRepository()
+        {
+            _restaurantRepository = new RestaurantRepository(_context);
         }
 
         [TearDown]
