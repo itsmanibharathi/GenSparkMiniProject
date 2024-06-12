e9b614e294cc08e9fd17e80142e3549fc8dcd0a4
Manibharathi <manibharathidct@gmail.com>
2024-06-12 15:55:23 +0530

    init the front-end with tailwind css

---
+++ src/Frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  purge: ['./views/**/*.html', './js/**/*.js'],
+  darkMode: false,
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
