cdd9eac2840ddc69c5f031f29242aa06beb7b551
Manibharathi <manibharathidct@gmail.com>
2024-06-15 17:31:55 +0530

    add auth template

--- src/Frontend/src/scripts/main.js
+++ src/Frontend/src/scripts/main.js
@@ -1,12 +1,15 @@
+import '../scripts/auth.js';
 const $ = require('jquery');
 import fontawesome from '@fortawesome/fontawesome-free/css/all.css';
 
 import homeComponent from '../views/home/home.html';
 import footer from '../partials/footer.html';
 
+import Component from '../views/customer/auth.html';
 
 $(document).ready(function () {
-  $("#root").load(homeComponent, function () {
-    $("#footer-placeholder").load(footer);
-  });
+  // $("#root").load(homeComponent, function () {
+  //   $("#footer-placeholder").load(footer);
+  // });
+  $('#root').load(Component);
 });
