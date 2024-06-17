ff8d81090385da57d85da14de8828386d4b6d685
Manibharathi <manibharathidct@gmail.com>
2024-06-17 19:48:21 +0530

    add product ui

--- src/Frontend/src/scripts/main.js
+++ src/Frontend/src/scripts/main.js
@@ -5,7 +5,7 @@ import fontawesome from '@fortawesome/fontawesome-free/css/all.css';
 import homeComponent from '../views/home/home.html';
 import footer from '../partials/footer.html';
 
-import Component from '../partials/card.html';
+import Component from '../partials/order.html';
 
 $(document).ready(function () {
   // $("#root").load(homeComponent, function () {
