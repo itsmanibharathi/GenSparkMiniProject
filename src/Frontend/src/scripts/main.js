2bd141cf6b8353ba62bfe03ad30854f5c17fd868
Manibharathi <manibharathidct@gmail.com>
2024-06-17 11:20:27 +0530

    card

--- src/Frontend/src/scripts/main.js
+++ src/Frontend/src/scripts/main.js
@@ -5,7 +5,7 @@ import fontawesome from '@fortawesome/fontawesome-free/css/all.css';
 import homeComponent from '../views/home/home.html';
 import footer from '../partials/footer.html';
 
-import Component from '../views/customer/auth.html';
+import Component from '../partials/card.html';
 
 $(document).ready(function () {
   // $("#root").load(homeComponent, function () {
