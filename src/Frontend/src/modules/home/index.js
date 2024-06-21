a56e8efe811c61c92d4a844d1e7f46e5af72bc0f
Manibharathi <manibharathidct@gmail.com>
2024-06-21 13:15:27 +0530

    rename import service to Service

--- src/Frontend/src/modules/home/index.js
+++ src/Frontend/src/modules/home/index.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import HomeHeader from '../../components/home-header.html';
 import HomeBody from './index.html'
 import Footer from '../../components/footer.html';
-import loadComponent from '../../services/loadComponent.js';
+import loadComponent from '../../Services/loadComponent.js';
 
 const loadHome = () => {
     console.log('Loading Home');
