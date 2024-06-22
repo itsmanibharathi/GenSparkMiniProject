68614e99528d9867ee2babfc6b5a8b0847036025
Manibharathi <manibharathidct@gmail.com>
2024-06-22 12:18:48 +0530

    update path

--- src/Frontend/src/main.js
+++ src/Frontend/src/main.js
@@ -2,7 +2,11 @@ const $ = require('jquery');
 import './styles/auth.css';
 import fontawesome from '@fortawesome/fontawesome-free/css/all.css';
 import loadRoutes from './routes.js';
+import dotenv from 'dotenv';
 
+dotenv.config();
+const API_URL = process.env.API_URL;
+console.log(API_URL);
 $(document).ready(() => {
   loadRoutes();
   $(document).on('click', 'nav a', function (e) {
--- src/Frontend/src/routes.js
+++ src/Frontend/src/routes.js
@@ -1,14 +1,19 @@
 import $ from 'jquery';
 import loadCustomer from './modules/customer/index.js';
 import loadHome from './modules/home/index.js';
+
 const routes = {
     '/': loadHome,
     '/customer': loadCustomer
 };
 const loadRoutes = () => {
     var path = window.location.pathname.toLocaleLowerCase();
-    path = path === '/' ? '/' : path.replace(/\/$/, '');
-    console.log(path);
+    const base = process.env.BASE_PATH.toLocaleLowerCase();
+    if (path.indexOf(base) === 0) {
+        path = path.replace(base, '');
+        path = path === '' ? '/' : path;
+    }
+
     if (routes[path]) {
         routes[path]();
     }
