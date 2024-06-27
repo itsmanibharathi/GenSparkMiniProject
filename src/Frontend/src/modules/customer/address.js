f43d18e330354f1d355c7c75ff68d3a9e25e0476
Manibharathi <manibharathidct@gmail.com>
2024-06-27 19:26:45 +0530

    add customer address form validation

--- src/Frontend/src/modules/customer/address.js
+++ src/Frontend/src/modules/customer/address.js
@@ -1,4 +1,4 @@
-import $ from 'jquery';
+import $, { type } from 'jquery';
 import AddressPage from './address.html'
 import showAlert from '../../Services/alertService';
 import log from '../../utility/loglevel';
@@ -13,8 +13,63 @@ const AddressCallback = async (api, localStorage) => {
         $('#addAddressButton').removeClass('hidden');
     });
 
+    $('#addressForm').on('change', 'select', (e) => {
+        e.preventDefault();
+        let select = e.target;
+        if (select.value == '') {
+            select.classList.add('border-red-500');
+            select.nextElementSibling.innerText = 'This field is required';
+        } else {
+            select.classList.remove('border-red-500');
+            select.classList.add('border-green-500');
+            select.nextElementSibling.innerText = '';
+        }
+    });
+
+    $('#addressForm').on('blur', 'input', (e) => {
+        e.preventDefault();
+        let input = e.target;
+        if (input.value == '' && input.required) {
+            input.classList.add('border-red-500');
+            input.nextElementSibling.innerText = 'This field is required';
+        }
+        // code 
+        else if (input.name == 'code' && input.value.length != 1) {
+            if (!/^[a-zA-Z]$/.test(input.value)) {
+                input.classList.add('border-red-500');
+                input.nextElementSibling.innerText = 'Code must be a single character a-z';
+            }
+        }
+        else if (input.name == 'zipCode' && input.value.length != 6) {
+            if (!/^[0-9]{6}$/.test(input.value)) {
+                input.classList.add('border-red-500');
+                input.nextElementSibling.innerText = 'Zip code must be 6 digits';
+            }
+        }
+        else {
+            input.classList.remove('border-red-500');
+            input.nextElementSibling.innerText = '';
+            if (input.value !== '') {
+                input.classList.add('border-green-500');
+            }
+        }
+
+
+
+        // if (e.target.value === '') {
+        //     e.target.classList.add('border-red-500'); 
+        //     e.target.nextElementSibling.innerText = 'This field is required';
+        // }
+        // else {
+        //     e.target.classList.remove('border-red-500');
+        // }
+    }
+    );
+
+
     $('#addressForm').submit(async (e) => {
         e.preventDefault();
+
         const formData = $('#addressForm').serializeArray();
         const data = {};
         formData.forEach((item) => {
