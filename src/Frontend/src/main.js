0ede3623d28b1b54702e9f479827985db7a3336a
Manibharathi <manibharathidct@gmail.com>
2024-06-27 09:28:46 +0530

    change env

--- src/Frontend/src/main.js
+++ src/Frontend/src/main.js
@@ -1,29 +1,9 @@
-// const $ = require('jquery');
 import './styles/auth.css';
-import fontawesome from '@fortawesome/fontawesome-free/css/all.css';
-// import loadRoutes from './routes.js';
-import dotenv from 'dotenv';
-dotenv.config();
-
-
-// $(document).ready(() => {
-//   loadRoutes();
-//   $(document).on('click', 'nav a', function (e) {
-//     e.preventDefault();
-//     const url = $(this).attr('href');
-//     history.pushState(null, '', url);
-//     loadRoutes();
-//   })
-//   $(window).on('popstate', function () {
-//     loadRoutes();
-//   });
-// });
-
-
 import $ from 'jquery';
 import { loadRoutes } from './Services/routerService';
-
-
+import fontawesome from '@fortawesome/fontawesome-free/css/all.css';
+import dotenv from 'dotenv';
+dotenv.config();
 
 $(document).ready(() => {
     loadRoutes();
